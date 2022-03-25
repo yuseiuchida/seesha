cdf71a6c3bdc695139f7fd2806561ca65009ca13
rq-yuseiuchida-at-321899771753 <yusei@Yuseis-MacBook-Air.local>
2022-03-25 22:09:10 +0900

    2nd commit

--- app/models/flavor.rb
+++
@@ -1,3 +0,0 @@
-class Flavor < ApplicationRecord
-	validates :name, presence: true
-end
--- db/migrate/20220325091136_create_flavors.rb
+++
@@ -1,9 +0,0 @@
-class CreateFlavors < ActiveRecord::Migration[6.1]
-  def change
-    create_table :flavors do |t|
-      t.string :name, null: false
-
-      t.timestamps
-    end
-  end
-end
